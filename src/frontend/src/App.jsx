7f00849082fcede85147fb36b0aefd52353593b7
Tim Daubenschütz <tim@daubenschuetz.de>
2024-09-07 14:51:10 +0200

    FC integration

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,33 +1,94 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "@farcaster/auth-kit/styles.css";
+import { providers } from "ethers";
+import { AuthKitProvider, SignInButton, useProfile } from "@farcaster/auth-kit";
+
+const config = {
+  // For a production app, replace this with an Optimism Mainnet
+  // RPC URL from a provider like Alchemy or Infura.
+  relay: "https://relay.farcaster.xyz",
+  rpcUrl: "https://mainnet.optimism.io",
+  domain: "example.com",
+  siweUri: "https://example.com/login",
+  provider: new providers.JsonRpcProvider(undefined, 10)
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <main style={{ fontFamily: 'Inter, "Inter Placeholder", sans-serif' }}>
+      {/* @ts-expect-error ethers version type incompat */}
+      <AuthKitProvider config={config}>
+        <div style={{ position: "fixed", top: "12px", right: "12px" }}>
+          <SignInButton />
+        </div>
+        <div style={{ paddingTop: "33vh", textAlign: "center" }}>
+          <h1>@farcaster/auth-kit + Vite</h1>
+          <p>
+            This example app shows how to use{" "}
+            <a
+              href="https://docs.farcaster.xyz/auth-kit/introduction"
+              target="_blank" rel="noreferrer"
+            >
+              Farcaster AuthKit
+            </a>{" "}
+            and{" "}
+            <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
+              Vite
+            </a>
+            .
+          </p>
+          <Profile />
+          <div>
+            <h2>Run this demo:</h2>
+            <div
+              style={{
+                margin: "0 auto",
+                padding: "24px",
+                textAlign: "left",
+                maxWidth: "640px",
+                backgroundColor: "#fafafa",
+                fontFamily: "monospace",
+                fontSize: "1.25em",
+                border: "1px solid #eaeaea",
+              }}
+            >
+              git clone https://github.com/farcasterxyz/auth-monorepo.git &&
+              <br />
+              cd auth-monorepo/examples/frontend-only &&
+              <br />
+              yarn install &&
+              <br />
+              yarn dev
+            </div>
+          </div>
+        </div>
+      </AuthKitProvider>
+    </main>
+  );
+}
+
+function Profile() {
+  const profile = useProfile();
+  const {
+    isAuthenticated,
+    profile: { fid, displayName, custody },
+  } = profile;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Dating 13</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      {isAuthenticated ? (
+        <div>
+          <p>
+            Hello, {displayName}! Your FID is {fid}.
+          </p>
+          <p>Your custody address is: </p>
+          <pre>{custody}</pre>
+        </div>
+      ) : (
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Click the "Sign in with Farcaster" button above, then scan the QR code
+          to sign in.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      )}
     </>
   );
 }
