8b3ab56ac186137420c7c13f4fec487c3b29dec8
Tim Daubenschütz <tim@daubenschuetz.de>
2024-09-07 14:18:55 +0200

    Host react output

--- src/index.mjs
+++ src/index.mjs
@@ -3,9 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(express.static("src/public"));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
